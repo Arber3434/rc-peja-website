c2d35e6dbd4a917b0d805e9ed0766176c0812719
KDIA3 <kosovodigitalassociation@gmail.com>
2022-02-05 16:41:01 +0100

    commented navigation links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,21 +7,21 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Rotary Club Peja</span>
     </Link>
-    <ul className={styles.navigation}>
+    {/* <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
           Home
         </Link>
       </li>
-      <li className={styles.navigationItem}>
+      {/* <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
           Blog
         </Link>
-      </li>
-    </ul>
-  </nav>
+      </li> */}
+  </ul> * /}
+  </nav >
 )
 
 export default Navigation
